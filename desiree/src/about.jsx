c190d9af46e2514a3531df132afa1a8948e9b046
Desiree Soronio <desireesoronio@gmail.com>
2025-06-16 22:35:26 +0800

    fixed css and animation

--- desiree/src/about.jsx
+++ desiree/src/about.jsx
@@ -3,21 +3,36 @@ function Home() {
         <div>
             <section className="home" id="home">
                 <div className="home-content">
-                    {/* INTRO */}
-                    <h1>Hello! <br></br> My name is <span>Desiree.</span></h1>
-                    <h3 className="text-animation">
-                        I'm a
-                        <span><div className="text-wrapper">
-                            <div className="box b1">Web Developer</div>
-                            <div className="box b2">Student Leader</div>
-                            <div className="box b3">Active Volunteer</div>
-                        </div></span>
-                    </h3>
-                    <p>A driven leader with a curious mind for the ever-evolving world of technology and innovation, blending logical problem-solving with creative expression. Passion lies in exploring the intersection of computing and the arts—bringing ideas to life through illustration, design, music, and storytelling. Whether immersed in lines of code or lines of prose, there's always a pursuit of meaning, growth, and purposeful creation.</p>
+                    <h1>Hello! <br /> My name is <span>Desiree.</span></h1>
+                    
+                    <div className="text-animation">
+                        <div className="static-text">
+                            I'm&nbsp;
+                        </div>
+                        <div className="text-wrapper">
+                            <span>
+                                <h3>
+                                    <span className="box b1">a Web Developer</span>
+                                    <span className="box b2">a Student Leader</span>
+                                    <span className="box b3">an Active Volunteer</span>
+                                </h3>
+                            </span>
+                        </div>
+                    </div>
 
-                    {/* BUTTONS */}
+
+
+
+                    <p className="home-text">
+                        A driven leader with a curious mind for the ever-evolving world of technology and innovation, blending logical problem-solving
+                        with creative expression. Passion lies in exploring the intersection of computing and the arts—bringing ideas to life through
+                        illustration, design, music, and storytelling. Whether immersed in lines of code or lines of prose, there's always a pursuit of meaning,
+                        growth, and purposeful creation.
+                    </p>
+
+
                     <div className="buttons">
-                        <a href="#" style={{ textDecoration: 'none', color: 'inherit' }}>Send a message</a>
+                        <a href="#" style={{ textDecoration: 'none', color: 'inherit' }}>Read more</a>
                     </div>
                 </div>
 
